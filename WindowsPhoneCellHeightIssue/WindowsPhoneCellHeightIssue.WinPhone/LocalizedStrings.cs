eb6fd02f35918c13abbcdced3b0895a990773cdf
Brian Foody <brian.foody@integranetgroup.com.au>
2016-03-29 08:34:00 +0800

    Adding repo to replicate issue

---
+++ WindowsPhoneCellHeightIssue/WindowsPhoneCellHeightIssue.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WindowsPhoneCellHeightIssue.WinPhone.Resources;
+
+namespace WindowsPhoneCellHeightIssue.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WindowsPhoneCellHeightIssue/WindowsPhoneCellHeightIssue/ListViewSample.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace WindowsPhoneCellHeightIssue
+{
+    public partial class ListViewSample : ContentPage
+    {
+        public ListViewSample()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            ShowModal.Clicked += (s, e) =>
+            {
+                Navigation.PushModalAsync(new ContentPage());
+            };
+
+        }
+
+        public List<string> Cards { get; set; } = new List<string> { "a", "b", "c", "d", "e", "f" };
+    }
+}
